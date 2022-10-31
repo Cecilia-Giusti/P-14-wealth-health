fcfbc76de3b9214fb8da63a3cce2eab98e44321a
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-10-31 07:55:35 +0100

    Feat: add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header>
+      <div className="banner">
+        <img src="./assets/images/logo1.png" alt="Logo wealth health" />
+        <p className="title">HRNET</p>
+      </div>
+      <nav>
+        <ul>
+          <NavLink to={"/"}>
+            <li>Create employee</li>
+          </NavLink>
+          <NavLink to={"/current-employees"}>
+            <li>Current employees</li>
+          </NavLink>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
