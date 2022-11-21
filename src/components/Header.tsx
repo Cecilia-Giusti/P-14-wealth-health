2909c91ac2583818f281c3a41ab1228b2d090ff4
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-11-21 17:19:29 +0100

    Style: update responsive to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,18 @@
 import { NavLink } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { handleOpenNav } from "../utils/handleOpenNav";
 
 const Header = () => {
+  const dispatch = useAppDispatch();
+  const navOpen = useAppSelector((state) => state.reponsive.openHeader);
   return (
     <header>
+      <button
+        id="buttonNavClose"
+        onClick={() => handleOpenNav(dispatch, navOpen)}
+      >
+        <i className="fa-solid fa-xmark"></i>
+      </button>
       <div className="banner">
         <img src="./assets/images/logo1.png" alt="Logo wealth health" />
         <p className="title">HRNET</p>
