bc9eba41aa787b63cbfb23df2fab5b702da2dec8
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-12-06 09:54:59 +0100

    Test: add button nav

--- src/components/ButtonNav.tsx
+++ src/components/ButtonNav.tsx
@@ -5,9 +5,10 @@ const ButtonNav = () => {
   const dispatch = useAppDispatch();
   const openHeader = useAppSelector((state) => state.reponsive.openHeader);
   return (
-    <div id="buttonNav" className="buttonNav">
+    <div id="buttonNav" className="buttonNav" data-testid="buttonNav">
       <button
         className="button__navbar"
+        data-testid="navigation-button"
         aria-label="Open nav"
         onClick={() => handleOpenNav(dispatch, openHeader)}
       >
