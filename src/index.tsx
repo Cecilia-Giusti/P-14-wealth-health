cb4b3505bf3ff09c4aa7fc43ab20f51a8b263c2e
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-12-06 09:54:46 +0100

    Add mock store

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import { store } from "./app/store";
+import { setupStore } from "./app/store";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const store = setupStore();
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
