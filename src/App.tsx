42a623728924dd87db3a05d6ad7234089668b593
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-12-09 05:54:28 +0100

    Refactor: update performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { lazy, useEffect, Suspense } from "react";
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useAppDispatch } from "./app/hooks";
 import ButtonNav from "./components/ButtonNav";
 import Header from "./components/Header";
+import CreateEmployee from "./pages/CreateEmployee";
+import EmployeeList from "./pages/EmployeeList";
 import Error from "./pages/Error";
 import { getUsers } from "./service/user";
-
-const CreateEmployee = lazy(() => import("./pages/CreateEmployee"));
-const EmployeeList = lazy(() => import("./pages/EmployeeList"));
 
 const App = () => {
   const dispatch = useAppDispatch();
@@ -18,18 +17,16 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Header />
-        <ButtonNav />
-        <Routes>
-          <Route path="/P-14-wealth-health/" element={<CreateEmployee />} />
-          <Route
-            path="/P-14-wealth-health/current-employees"
-            element={<EmployeeList />}
-          />
-          <Route path="/P-14-wealth-health/*" element={<Error />} />
-        </Routes>
-      </Suspense>
+      <Header />
+      <ButtonNav />
+      <Routes>
+        <Route path="/P-14-wealth-health/" element={<CreateEmployee />} />
+        <Route
+          path="/P-14-wealth-health/current-employees"
+          element={<EmployeeList />}
+        />
+        <Route path="/P-14-wealth-health/*" element={<Error />} />
+      </Routes>
     </BrowserRouter>
   );
 };
