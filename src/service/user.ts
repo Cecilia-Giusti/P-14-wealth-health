ed699722fe65ab550a3d67da4be50a2f2bae6351
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-12-19 16:08:36 +0100

    Feat: add modelisation class

--- src/service/user.ts
+++ src/service/user.ts
@@ -2,9 +2,9 @@ import { AnyAction } from "@reduxjs/toolkit";
 import axios from "axios";
 import { Dispatch } from "react";
 import { setUsersData, addUser } from "../feature/usersSlice";
+import { User } from "../model/User";
 
 import { newEmployeeInt } from "../types/models";
-import { User } from "../model/User";
 
 const ADRESSE = "http://localhost:3004";
 
@@ -12,14 +12,13 @@ export const getUsers = (dispatch: Dispatch<AnyAction>) => {
   try {
     axios.get(`${ADRESSE}/users`).then((res) => {
       let newDataUsers: newEmployeeInt[] = [];
-      let dataUsers = res.data;
-
-      for (let i = 0; i >= dataUsers.length; i++) {
-        let newUser = new User(dataUsers[i]);
+      let datausers = res.data;
+      for (let i = 0; i >= datausers.length; i++) {
+        let newUser = new User(datausers[i]);
         newDataUsers.push(newUser);
       }
 
-      return dispatch(setUsersData(newDataUsers));
+      return dispatch(setUsersData(datausers));
     });
   } catch (error) {
     console.error(error);
