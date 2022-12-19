dc8e743d9280d256936c876fbef683816de851ef
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-12-19 16:07:43 +0100

    Feat: add error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { handleOpenNav } from "../utils/handleOpenNav";
 
 const Error = () => {
-  return <div>Error</div>;
+  const dispatch = useAppDispatch();
+  const openHeader = useAppSelector((state) => state.reponsive.openHeader);
+  return (
+    <main
+      data-testid="main-errorPage"
+      className={openHeader ? "main errorPage" : "main errorPage close"}
+      onClick={() => {
+        handleOpenNav(dispatch, true);
+      }}
+    >
+      <h1> Error 404 </h1>
+      <h2> Page not Found</h2>
+    </main>
+  );
 };
 
 export default Error;
