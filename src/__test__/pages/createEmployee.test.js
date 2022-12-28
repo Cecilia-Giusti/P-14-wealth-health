d322b62bc8884f4fe7a04fb477e92b8dfa58d4f1
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-12-28 10:53:23 +0100

    Test: add createEmployee page test

---
+++ src/__test__/pages/createEmployee.test.js
@@ -0,0 +1,25 @@
+import { screen } from "@testing-library/react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { setupStore } from "../../app/store";
+import CreateEmployee from "../../pages/CreateEmployee";
+import { renderWithProviders } from "../../utils/test-utils";
+
+describe("Given the user is on the form", () => {
+  describe("When the form is visible", () => {
+    test("Then the title is visible", async () => {
+      //Store
+      const store = setupStore();
+      renderWithProviders(
+        <BrowserRouter>
+          <Route path="/P-14-wealth-health/" element={<CreateEmployee />} />
+        </BrowserRouter>,
+        { store }
+      );
+
+      //Title
+      const title = screen.getElementByTagName("h1");
+      expect(title).toBeTruthy();
+      expect(title).toHaveTextContent("Create Employee");
+    });
+  });
+});
