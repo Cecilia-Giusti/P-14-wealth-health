1905e94f951cc7f865fe4d15017e0356b7f18643
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2023-01-18 11:49:12 +0100

    Tests: add tests to app

---
+++ src/__test__/app.test.js
@@ -0,0 +1,47 @@
+import { screen } from "@testing-library/react";
+
+import App from "../../src/App";
+import { setupStore } from "../app/store";
+import { renderWithProviders } from "../utils/test-utils";
+
+jest.mock("../service/user", () => {
+  const getFN = jest.fn(() => true);
+  const thenFN = jest.fn();
+  const catchFN = jest.fn();
+
+  return {
+    getUsers: getFN,
+    thenFN,
+    catchFN,
+  };
+});
+
+jest.mock("@cecigiu2b/dropdown-menu-react", () => {
+  const fakeName = jest.fn(({ name }) => name);
+  const fakeOptions = jest.fn(({ options }) => options);
+  const fakeCustomClassSelect = jest.fn(
+    ({ customClassSelect }) => customClassSelect
+  );
+  const fakeCustomClassOption = jest.fn(
+    ({ customClassOption }) => customClassOption
+  );
+  const fakGetValue = jest.fn(({ getValue }) => getValue);
+
+  return {
+    DropdownMenu: fakeName,
+    fakeOptions,
+    fakeCustomClassSelect,
+    fakeCustomClassOption,
+    fakGetValue,
+  };
+});
+
+describe("App", () => {
+  it("renders correctly and calls getUsers with dispatch", () => {
+    //Store
+    const store = setupStore();
+    renderWithProviders(<App />, { store });
+    expect(screen.getByTestId("header")).toBeInTheDocument();
+    expect(screen.getByTestId("buttonNav")).toBeInTheDocument();
+  });
+});
